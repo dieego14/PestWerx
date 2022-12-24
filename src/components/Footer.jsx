97f0362e9f8060e03290cb96e4fbcfb4fca90e7a
Diego <dieegohernandez14@gmail.com>
2022-12-23 17:59:06 -0800

    Init Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = ()=>{
+
+
+
+    return (
+        <div className="max-w-[1670px] ">
+            <div className="max-h-[500px]">
+                <h1>Hello</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
