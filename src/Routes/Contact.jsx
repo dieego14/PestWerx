17ac0c36cc8921925b29a1d8838063a869182b89
Diego <dieegohernandez14@gmail.com>
2022-12-23 23:53:18 -0800

    Create Contact page

---
+++ src/Routes/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Services</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Contact;
