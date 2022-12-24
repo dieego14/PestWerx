0f330b2fa1b3c25adf72a4b3f20cd348f50cbc08
Diego <dieegohernandez14@gmail.com>
2022-12-23 18:34:32 -0800

    Improve Look

--- src/Adv.jsx
+++ src/Adv.jsx
@@ -4,7 +4,7 @@ import { MdOutlineInventory2, MdOutlineLocationOn } from "react-icons/md";
 
 const Adv = () => {
   return (
-    <div className="max-w-[1640px] mx-auto p-4 py-12 bg-blue-200">
+    <div className="max-w-[1920px] mx-auto p-4 py-12 bg-blue-200">
       <div className="max-h-[500px]">
         <div>
           <h1 className="text-4xl sm:text-5xl font-myFont text-center">
@@ -25,7 +25,8 @@ const Adv = () => {
         </div>
         {/* Info Cards */}
         <div className="grid grid-cols-3 gap-5 py-2">
-          <div className="bg-slate-400 px-4 py-5 rounded-lg">
+            {/* First Card */}
+          <div className="bg-slate-400 px-4 py-5 rounded-lg hover:bg-yellow-400">
             <div className="flex justify-center py-6 border-b-2 sm:border-b-4 border-black">
               <BsStar size={40} />
             </div>
@@ -34,7 +35,8 @@ const Adv = () => {
               <p className="text-center">5 Star Service <span className="text-red-600 font-extrabold">Guaranteed</span></p>
             </div>
           </div>
-          <div className="bg-slate-400 px-4 py-5 rounded-lg">
+          {/* Second Card */}
+          <div className="bg-slate-400 px-4 py-5 rounded-lg hover:bg-yellow-400">
             <div className="flex justify-center py-6 border-b-2 sm:border-b-4 border-black">
               <MdOutlineInventory2 size={40} />
             </div>
@@ -43,7 +45,8 @@ const Adv = () => {
               <p className="text-center">Numerous Pest Services</p>
             </div>
           </div>
-          <div className="bg-slate-400 px-4 py-5 rounded-lg">
+           {/* Third Card */}
+          <div className="bg-slate-400 px-4 py-5 rounded-lg hover:bg-yellow-400">
             <div className="flex justify-center py-6 border-b-2 sm:border-b-4 border-black">
               <MdOutlineLocationOn size={40} />
             </div>
