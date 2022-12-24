8959a6e199f70ae4394bd3e6ffa207e33caecbe6
Diego <dieegohernandez14@gmail.com>
2022-12-23 23:53:28 -0800

    Create Home page route

---
+++ src/Routes/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Adv from "../components/Adv"
+import Footer from "../components/Footer"
+import Hero from "../components/Hero"
+
+const Home = () =>{
+
+    return(
+
+        <>
+        <Hero/>
+        <Adv />
+        </>
+    )
+}
+export default Home;
