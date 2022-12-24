e783b5b33595008abeaea587291cfc98cef9b625
Diego <dieegohernandez14@gmail.com>
2022-12-23 23:51:50 -0800

    Create and set up Routes with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+// Components:
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Adv from "./Adv";
 import Footer from "./components/Footer";
+// Routes:
+import Home from "./Routes/Home";
+import About from "./Routes/About";
+import Services from "./Routes/Services";
+import Contact from "./Routes/Contact";
+import Locations from "./Routes/Locations";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <Hero />
-      <Adv />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Locations" element={<Locations />} />
+      </Routes>
       <Footer />
-    </div>
+      {/* <Hero />
+      <Adv />
+      <Footer /> */}
+    </>
   );
 }
 
