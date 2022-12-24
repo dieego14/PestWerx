7aacff800279cd48212860b0a43fe2e183fe4366
Diego <dieegohernandez14@gmail.com>
2022-12-23 23:53:05 -0800

    Create About page

---
+++ src/Routes/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const About = () => {
+  return (
+    <div>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </div>
+  );
+};
+
+export default About;
