35a3837b40d03406cb5db3ed616d6cd599066389
Diego <dieegohernandez14@gmail.com>
2022-12-23 23:53:59 -0800

    Create Location page

---
+++ src/Routes/Locations.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Locations = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Services</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Locations;
