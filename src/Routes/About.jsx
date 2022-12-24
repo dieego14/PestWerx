aa20d9bac1af220ddf3aad944d6d199a693b911c
Diego <dieegohernandez14@gmail.com>
2022-12-24 01:24:10 -0800

    Create About us page

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -4,9 +4,14 @@ import Navbar from "../components/Navbar";
 
 const About = () => {
   return (
-    <div>
-      <div>
-        <h1>Hello</h1>
+    <div className="max-w-[1920px] bg-gray-600">
+      <div className="max-h-[600px]">
+        {/* About Us */}
+        <div className="py-5">
+          <h1 className="font-myFont text-cneter text-4xl">
+            ABOUT US
+          </h1>
+        </div>
       </div>
     </div>
   );
