07f64834519cb33f9953108ab93056b32077bc59
Diego <dieegohernandez14@gmail.com>
2022-12-23 18:34:39 -0800

    Create Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,23 @@ const Footer = ()=>{
 
 
     return (
-        <div className="max-w-[1670px] ">
+        <div className="max-w-[1920px] mx-auto bg-black/80">
             <div className="max-h-[500px]">
-                <h1>Hello</h1>
+               <div className="text-white text-center font-sans flex flex-col gap-4 py-4">
+                    {/* Footer Info */}
+                    <h3 className="font-bold">
+                        PESTWERX
+                    </h3>
+                    <p className="">
+                        18565 SOLEDAD CANYON ROAD #352 CANYON COUNTRY, CA. 91351
+                    </p>
+                    <p>
+                        (800)283-8290
+                    </p>
+                    <p className=" text-xs">
+                        COPYRIGHT © 2022 PESTWERX - ALL RIGHTS RESERVED.
+                    </p>
+               </div>
             </div>
         </div>
     )
