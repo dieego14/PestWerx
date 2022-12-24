cd302e2c9ea7e6fd5f9d1c902741844e80bfbc98
Diego <dieegohernandez14@gmail.com>
2022-12-24 03:05:01 -0800

    Add to About us

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -4,13 +4,21 @@ import Navbar from "../components/Navbar";
 
 const About = () => {
   return (
-    <div className="max-w-[1920px] bg-gray-600">
-      <div className="max-h-[600px]">
+    <div className="max-w-[1920px] bg-myColor">
+      <div className="max-h-[1000px]">
         {/* About Us */}
-        <div className="py-5">
-          <h1 className="font-myFont text-cneter text-4xl">
-            ABOUT US
+        <div className>
+          <img className="w-full max-h-[300px] object-cover"
+            src="https://img1.wsimg.com/isteam/stock/2141/:/rs=w:1350,m"
+           />
+           <h1 className=" py-5 font-myFont text-center text-6xl">
+            WHO WE ARE
           </h1>
+          <div>
+            <p className="p-16 flex justify-center text-3xl">
+             Founded in and servicing the Los Angeles Area. We find and manege
+            </p>
+          </div>
         </div>
       </div>
     </div>
